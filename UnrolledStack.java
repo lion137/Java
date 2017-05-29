0a5fbc822266754a01f6a8b56ec44aca3920c1f2
lion137 <lion137@gmail.com>
2017-05-29 20:39:50 +0100

    Update UnrolledStack.java

--- UnrolledStack.java
+++ UnrolledStack.java
@@ -1,90 +1,101 @@
 package part02;
 
-import java.util.ArrayList;
-
 /**
- * Created by lion on 26/05/17.
+ * Created by lion on 28/05/17.
  */
-public class UnrolledStack {
+public class UnrolledStackArray {
     private Node first;
     int length = 0;
     int max_elements;
+    int top = 0;
+    boolean cnt = false;
 
-    public UnrolledStack(int max){
+    public UnrolledStackArray(int max) {
         max_elements = max;
     }
 
 
-    private class Node{
-        ArrayList<Integer> data = new ArrayList<Integer>();
+    private class Node {
+        Object[] data = new Object[max_elements];
         Node next;
-        int non_zero = max_elements;
+
     }
 
-
-
-    public void push(int item){
+    public void push(int item) {
         if (first != null) {
-            if (first.non_zero > 0){
-                first.data.add(item);
-                first.non_zero -= 1;
-            }
-            else {
+            if (top == max_elements) {
                 Node old = first;
                 first = new Node();
-
-                first.data.add(item);
-                first.non_zero -= 1;
+                top = 0;
+                first.data[top] = item;
                 first.next = old;
+                top++;
+            } else {
+                first.data[top] = item;
+                top++;
             }
-        }
-        else{
+        } else {
             Node old = first;
             first = new Node();
-            first.data.add(item);
-            first.non_zero -= 1;
+            top = 0;
+            first.data[top] = item;
             first.next = old;
-            
+            top++;
         }
         length += 1;
+        cnt = false;
     }
 
-    public Object pop(){
+    public Object pop() {
         return this.popI();
     }
 
-    private Object popI(){
-        if (this.isEmpty()) {return null;}
-        else {
-            if (first.data.isEmpty()) {
-                first = first.next;
-                if (first == null) {return null;}
+    private Object popI() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            if (top >= max_elements) {
+                if (first == null) {
+                    return null;
+                }
                 length -= 1;
-                return first.data.remove(first.data.size() - 1);
+                top = max_elements - 1;
+                Object tmp = first.data[top];
+                first.data[top] = null;
+                top--;
+                return tmp;
             } else {
+                if (top == -1) {
+                    first = first.next;
+                    if (first == null) {
+                        return null;
+                    }
+                    length -= 1;
+                    top = max_elements - 1;
+                    Object tmp = first.data[top];
+                    first.data[top] = null;
+                    top--;
+                    return tmp;
+                }
+                if (first.data[top] == null) {
+                    top--;
+                }
                 length -= 1;
-                return first.data.remove(first.data.size() - 1);
+                Object tmp = first.data[top];
+                first.data[top] = null;
+                top--;
+                return tmp;
+
             }
         }
 
     }
 
-    public boolean isEmpty(){
-        return first == null;
+    public boolean isEmpty() {
+        return length == 0;
     }
 
-    public int getLength(){
+    public int getLength() {
         return length;
     }
-
-    public void printUnrolledStack(){
-
-            Node current;
-            current = first;
-            while (current != null) {
-                System.out.print(current.data);
-                current = current.next;
-            }
-    }
-
 }
